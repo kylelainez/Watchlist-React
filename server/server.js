03ccb30018506ebb94163ceb495eee3e873a7a1d
kylelainez <kyleadrianlainez@gmail.com>
2021-04-30 10:27:55 -0700

    Move server.js

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const path = require('path');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 const cors = require('cors');
-const routes = require('./server/routes');
+const routes = require('./routes');
 const session = require('express-session');
-const passport = require('passport')
+const passport = require('passport');
 
 const app = express();
 
-require('./server/database');
-require('./server/passport');
+require('./database');
+require('./passport');
 
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,30 +20,30 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(
-	session({
-		secret: 'React Watchlist!',
-		resave: false,
-		saveUninitialized: true
-	})
+    session({
+        secret: 'React Watchlist!',
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function (req, res, next) {
-	console.log(res.locals, 'this');
-	res.locals.user = req.user;
-	next();
-});
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    console.log(res.locals, 'this');
+    res.locals.user = req.user;
+    next();
 });
 
 app.get('/api/', routes);
 
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
-  console.log(`Express app listening on port ${port}`);
+app.listen(port, function () {
+    console.log(`Express app listening on port ${port}`);
 });
