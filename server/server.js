c3e17901065f5b4a3944cfe18ed7b9f67b4ac7ba
kylelainez <kyleadrianlainez@gmail.com>
2021-05-01 20:37:44 -0700

    Move to JWT

--- server/server.js
+++ server/server.js
@@ -4,13 +4,9 @@ const path = require('path');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
 const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-
 const app = express();
 
 require('./database');
-require('./passport');
 
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger('dev'));
@@ -18,25 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(
-    session({
-        secret: 'React Watchlist!',
-        resave: false,
-        saveUninitialized: true,
-    })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(function (req, res, next) {
-    console.log(res.locals, 'this');
-    res.locals.user = req.user;
-    next();
-});
 app.use('/api', require('./routes'));
 app.get('/*', function (req, res) {
-    console.log('here');
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,20 +10,13 @@ import {
 } from 'react-bootstrap';
 
 export default function NavBar() {
-    const test = () => {
-        axios.get('/api/auth/google', {
-            withCredentials: true,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        });
-    };
-
     return (
         <Navbar bg='light' expand='lg'>
             <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
                 <Nav className='ml-auto pr-2'>
-                    <Nav.Link onClick={test}>Home</Nav.Link>
+                    <Nav.Link onClick={}>Home</Nav.Link>
                     <Nav.Link href='#link'>Link</Nav.Link>
                 </Nav>
                 <Form inline>
