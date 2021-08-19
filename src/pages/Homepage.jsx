95851317f520022362bbdfab1f37310c5340165e
kylelainez <kyleadrianlainez@gmail.com>
2021-08-19 16:46:51 -0700

    Add Background

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,9 +3,13 @@ import { Container, Row, Image, Col, Button } from 'react-bootstrap';
 import TMDB from './../tmdb.svg';
 import LoginForm from './../components/LoginForm';
 import SignupForm from './../components/SignupForm';
+import BGImage from './../images/movie-bg.jpeg';
 
 export default function Homepage({ handleUser }) {
     const [selected, setSelected] = useState('home');
+
+    const link =
+        'https://unsplash.com/photos/n7A4TjAQLN4?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink';
 
     const setView = () => {
         switch (selected) {
@@ -61,41 +65,65 @@ export default function Homepage({ handleUser }) {
     };
 
     return (
-        <Container
-            className='d-flex'
-            style={{
-                height: 'calc(100vh - 56px)',
-                minHeight: '820px',
-                position: 'relative',
-                color: '#fff',
-            }}>
-            <Col
-                className='justify-self-center align-self-center py-4'
+        <>
+            <div
                 style={{
-                    backgroundColor: '#101010',
-                    borderRadius: '30px',
-                    color: '#CFCFCF',
-                }}>
-                <Row className='justify-content-center'>
-                    <h1>Watchlist React</h1>
-                </Row>
-                {setView()}
-            </Col>
-
-            <Col
-                style={{
-                    position: 'absolute',
-                    bottom: 0,
+                    backgroundImage: `url(${BGImage})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundOrigin: 'content-box',
+                    backgroundSize: 'cover',
+                    top: 0,
                     left: 0,
-                    zIndex: 1030,
+                    right: 0,
+                    bottom: 0,
+                    position: 'absolute',
                 }}>
-                <Row className='justify-content-center px-5 pb-3'>
-                    <Image src={TMDB} style={{ width: '300px' }} />
-                </Row>
-                <Row className='justify-content-center'>
-                    <p>Powered by The Movie Database API</p>
-                </Row>
-            </Col>
-        </Container>
+                <div
+                    style={{
+                        backgroundColor: 'black',
+                        opacity: 0.7,
+                        height: '100%',
+                        width: '100%',
+                        zIndex: 1,
+                    }}
+                />
+            </div>
+            <Container
+                className='d-flex'
+                style={{
+                    height: 'calc(100vh - 60px)',
+                    minHeight: '600px',
+                    position: 'relative',
+                    color: '#fff',
+                }}>
+                <Col
+                    className='justify-self-center align-self-center py-4'
+                    style={{
+                        backgroundColor: '#101010',
+                        borderRadius: '30px',
+                        color: '#CFCFCF',
+                    }}>
+                    <Row className='justify-content-center'>
+                        <h1>Watchlist React</h1>
+                    </Row>
+                    {setView()}
+                </Col>
+
+                <Col
+                    style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        left: 0,
+                        zIndex: 1030,
+                    }}>
+                    <Row className='justify-content-center px-5 pb-3'>
+                        <Image src={TMDB} style={{ width: '300px' }} />
+                    </Row>
+                    <Row className='justify-content-center'>
+                        <p>Powered by The Movie Database API</p>
+                    </Row>
+                </Col>
+            </Container>
+        </>
     );
 }
