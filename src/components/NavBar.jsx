f5cbf1315a1cce783682bc9c19e677224ae74335
kylelainez <kyleadrianlainez@gmail.com>
2021-09-20 18:59:03 -0700

    NavBar Fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import {
+    Navbar,
+    Nav,
+    Form,
+    FormControl,
+    Button,
+    Row,
+    Col,
+} from 'react-bootstrap';
 
 export default function NavBar() {
     return (
@@ -10,18 +18,26 @@ export default function NavBar() {
             style={{ padding: '8px 30px' }}>
             <Navbar.Brand href='#home'>Watchlist React</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
+            <Navbar.Collapse
+                id='basic-navbar-nav'
+                className='justify-content-end'>
                 <Nav className='ml-auto pr-2'>
                     <Nav.Link>Home</Nav.Link>
                     <Nav.Link href='#link'>Link</Nav.Link>
                 </Nav>
-                <Form inline='true'>
-                    <FormControl
-                        type='text'
-                        placeholder='Search'
-                        className='mr-sm-2'
-                    />
-                    <Button variant='outline-success'>Search</Button>
+                <Form>
+                    <Row>
+                        <Col>
+                            <FormControl
+                                type='text'
+                                placeholder='Search'
+                                className='mr-sm-2'
+                            />
+                        </Col>
+                        <Col md='auto'>
+                            <Button variant='outline-success'>Search</Button>
+                        </Col>
+                    </Row>
                 </Form>
             </Navbar.Collapse>
         </Navbar>
